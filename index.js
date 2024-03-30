157020cb224861f72ab9ac7093bd2af4907a70af
YuriAmani <yuriamani7@gmail.com>
2024-03-30 11:19:12 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let charges
+  if(feet <= 400){
+      charges = 'This one is on me!' 
+    }
+    else if (feet > 400 && feet < 2000) {
+      charges = 'That will be twenty bucks.';
+    }else if (feet >= 2000 && feet <2500) {
+      charges = 'I will gladly take your thirty bucks.'
+    }else {
+      charges = 'No can do.'
+    }
+    
+  return charges
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.'
+        break;
+    case 'not as generous':
+        return 'Thank you.'
+        break;
+    default:
+        return 'Bye.'
+}
 }
